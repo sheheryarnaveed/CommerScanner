a64a6a6ea778de0eebc14cc43ed02a25fc209ab9
Sheheryar Naveed <34706398+sheheryarnaveed@users.noreply.github.com>
2020-04-14 13:28:37 +0800

    Update concat.sh

--- concat.sh
+++ concat.sh
@@ -1,26 +1,26 @@
-zcat human_pancreas_R1.fastq.gz | head -n 507242880 > human5_mouse5_R1.fastq
-zcat human_pancreas_R2.fastq.gz | head -n 507242880 > human5_mouse5_R2.fastq
-zcat mouse_pancreas_R1.fastq.gz | head -n 1168336276 >> human5_mouse5_R1.fastq
-zcat mouse_pancreas_R2.fastq.gz | head -n 1168336276 >> human5_mouse5_R2.fastq
+zcat SRR7276477_R1.fastq | head -n 917856724 > human5_mouse5_R1.fastq
+zcat SRR7276477_R2.fastq | head -n 917856724 > human5_mouse5_R2.fastq
+zcat mouse_liver_R1.fastq | head -n 1872729372 >> human5_mouse5_R1.fastq
+zcat mouse_liver_R2.fastq | head -n 1872729372 >> human5_mouse5_R2.fastq
 
-zcat human_pancreas_R1.fastq.gz | head -n 405794304 > human4_mouse6_R1.fastq
-zcat human_pancreas_R2.fastq.gz | head -n 405794304 > human4_mouse6_R2.fastq
-zcat mouse_pancreas_R1.fastq.gz | head -n 1402003524 >> human4_mouse6_R1.fastq
-zcat mouse_pancreas_R2.fastq.gz | head -n 1402003524 >> human4_mouse6_R2.fastq
+zcat SRR7276477_R1.fastq | head -n 734285376 > human4_mouse6_R1.fastq
+zcat SRR7276477_R2.fastq | head -n 734285376 > human4_mouse6_R2.fastq
+zcat mouse_liver_R1.fastq | head -n 2247275244 >> human4_mouse6_R1.fastq
+zcat mouse_liver_R2.fastq | head -n 2247275244 >> human4_mouse6_R2.fastq
 
-zcat human_pancreas_R1.fastq.gz | head -n 304345728 > human3_mouse7_R1.fastq
-zcat human_pancreas_R2.fastq.gz | head -n 304345728 > human3_mouse7_R2.fastq
-zcat mouse_pancreas_R1.fastq.gz | head -n 1635670764 >> human3_mouse7_R1.fastq
-zcat mouse_pancreas_R2.fastq.gz | head -n 1635670764 >> human3_mouse7_R2.fastq
+zcat SRR7276477_R1.fastq | head -n 550714032 > human3_mouse7_R1.fastq
+zcat SRR7276477_R2.fastq | head -n 550714032 > human3_mouse7_R2.fastq
+zcat mouse_liver_R1.fastq | head -n 2621821104 >> human3_mouse7_R1.fastq
+zcat mouse_liver_R2.fastq | head -n 2621821104 >> human3_mouse7_R2.fastq
 
-zcat human_pancreas_R1.fastq.gz | head -n 202897152 > human2_mouse8_R1.fastq
-zcat human_pancreas_R2.fastq.gz | head -n 202897152 > human2_mouse8_R2.fastq
-zcat mouse_pancreas_R1.fastq.gz | head -n 1869338016 >> human2_mouse8_R1.fastq
-zcat mouse_pancreas_R2.fastq.gz | head -n 1869338016 >> human2_mouse8_R2.fastq
+zcat SRR7276477_R1.fastq | head -n 367142688 > human2_mouse8_R1.fastq
+zcat SRR7276477_R2.fastq | head -n 367142688 > human2_mouse8_R2.fastq
+zcat mouse_liver_R1.fastq | head -n 2996366976 >> human2_mouse8_R1.fastq
+zcat mouse_liver_R2.fastq | head -n 2996366976 >> human2_mouse8_R2.fastq
 
-zcat human_pancreas_R1.fastq.gz | head -n 101448576 > human1_mouse9_R1.fastq
-zcat human_pancreas_R2.fastq.gz | head -n 101448576 > human1_mouse9_R2.fastq
-zcat mouse_pancreas_R1.fastq.gz | head -n 2103005268 >> human1_mouse9_R1.fastq
-zcat mouse_pancreas_R2.fastq.gz | head -n 2103005268 >> human1_mouse9_R2.fastq
+zcat SRR7276477_R1.fastq | head -n 183571344 > human1_mouse9_R1.fastq
+zcat SRR7276477_R2.fastq | head -n 183571344 > human1_mouse9_R2.fastq
+zcat mouse_liver_R1.fastq | head -n 3370912848 >> human1_mouse9_R1.fastq
+zcat mouse_liver_R2.fastq | head -n 3370912848 >> human1_mouse9_R2.fastq
 
 pigz human*
