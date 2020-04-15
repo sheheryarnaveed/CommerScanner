1858dcc2bbf87c7526a1b3de50a48dcf9971c378
Sheheryar Naveed <34706398+sheheryarnaveed@users.noreply.github.com>
2020-04-15 14:05:38 +0800

    Update concat.sh

--- concat.sh
+++ concat.sh
@@ -1,26 +1,26 @@
-zcat SRR7276477_R1.fastq | head -n 917856724 > human5_mouse5_R1.fastq
-zcat SRR7276477_R2.fastq | head -n 917856724 > human5_mouse5_R2.fastq
-zcat mouse_liver_R1.fastq | head -n 1872729372 >> human5_mouse5_R1.fastq
-zcat mouse_liver_R2.fastq | head -n 1872729372 >> human5_mouse5_R2.fastq
+zcat human_testis_R1.fastq | head -n 618735460 > human5_mouse5_R1.fastq
+zcat human_testis_R2.fastq | head -n 618735460 > human5_mouse5_R2.fastq
+zcat mouse_testis_R1.fastq | head -n 744279980 >> human5_mouse5_R1.fastq
+zcat mouse_testis_R2.fastq | head -n 744279980 >> human5_mouse5_R2.fastq
 
-zcat SRR7276477_R1.fastq | head -n 734285376 > human4_mouse6_R1.fastq
-zcat SRR7276477_R2.fastq | head -n 734285376 > human4_mouse6_R2.fastq
-zcat mouse_liver_R1.fastq | head -n 2247275244 >> human4_mouse6_R1.fastq
-zcat mouse_liver_R2.fastq | head -n 2247275244 >> human4_mouse6_R2.fastq
+zcat human_testis_R1.fastq | head -n 494988368 > human4_mouse6_R1.fastq
+zcat human_testis_R2.fastq | head -n 494988368 > human4_mouse6_R2.fastq
+zcat mouse_testis_R1.fastq | head -n 893135976 >> human4_mouse6_R1.fastq
+zcat mouse_testis_R2.fastq | head -n 893135976 >> human4_mouse6_R2.fastq
 
-zcat SRR7276477_R1.fastq | head -n 550714032 > human3_mouse7_R1.fastq
-zcat SRR7276477_R2.fastq | head -n 550714032 > human3_mouse7_R2.fastq
-zcat mouse_liver_R1.fastq | head -n 2621821104 >> human3_mouse7_R1.fastq
-zcat mouse_liver_R2.fastq | head -n 2621821104 >> human3_mouse7_R2.fastq
+zcat human_testis_R1.fastq | head -n 371241276 > human3_mouse7_R1.fastq
+zcat human_testis_R2.fastq | head -n 371241276 > human3_mouse7_R2.fastq
+zcat mouse_testis_R1.fastq | head -n 1041991972 >> human3_mouse7_R1.fastq
+zcat mouse_testis_R2.fastq | head -n 1041991972 >> human3_mouse7_R2.fastq
 
-zcat SRR7276477_R1.fastq | head -n 367142688 > human2_mouse8_R1.fastq
-zcat SRR7276477_R2.fastq | head -n 367142688 > human2_mouse8_R2.fastq
-zcat mouse_liver_R1.fastq | head -n 2996366976 >> human2_mouse8_R1.fastq
-zcat mouse_liver_R2.fastq | head -n 2996366976 >> human2_mouse8_R2.fastq
+zcat human_testis_R1.fastq | head -n 247494184 > human2_mouse8_R1.fastq
+zcat human_testis_R2.fastq | head -n 247494184 > human2_mouse8_R2.fastq
+zcat mouse_testis_R1.fastq | head -n 1190847968 >> human2_mouse8_R1.fastq
+zcat mouse_testis_R2.fastq | head -n 1190847968 >> human2_mouse8_R2.fastq
 
-zcat SRR7276477_R1.fastq | head -n 183571344 > human1_mouse9_R1.fastq
-zcat SRR7276477_R2.fastq | head -n 183571344 > human1_mouse9_R2.fastq
-zcat mouse_liver_R1.fastq | head -n 3370912848 >> human1_mouse9_R1.fastq
-zcat mouse_liver_R2.fastq | head -n 3370912848 >> human1_mouse9_R2.fastq
+zcat human_testis_R1.fastq | head -n 123747092 > human1_mouse9_R1.fastq
+zcat human_testis_R2.fastq | head -n 123747092 > human1_mouse9_R2.fastq
+zcat mouse_testis_R1.fastq | head -n 1339703964 >> human1_mouse9_R1.fastq
+zcat mouse_testis_R2.fastq | head -n 1339703964 >> human1_mouse9_R2.fastq
 
 pigz human*
